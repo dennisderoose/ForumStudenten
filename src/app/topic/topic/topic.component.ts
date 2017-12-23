b324f451cc5a2affe38d78ff3b1eecbe90aa3399
dennisderoose <dennisderoose@hotmail.com>
2017-12-23 18:14:04 +0100

    proberen opmerking toevoegen

--- src/app/topic/topic/topic.component.ts
+++ src/app/topic/topic/topic.component.ts
@@ -53,7 +53,7 @@ export class TopicComponent implements OnInit {
     opmerking(form:any) { 
       console.log(form['opmerking']); 
       console.log(this.model);
-      console.log(this.opmerkingtoevoegen.value.topic); 
+      console.log(this.opmerkingtoevoegen); 
       console.log(this.opmerkingtoevoegen.value.opmerkingname);    
       let nextArray = [];
       /*const topic = new Topic("kop","hln");
