4a07cbf5359c91d6401f56a152060d11044736b1
dennisderoose <dennisderoose@hotmail.com>
2017-12-24 18:08:23 +0100

    Opmerking toevoegen

--- models/Topic.js
+++ models/Topic.js
@@ -1,11 +1,6 @@
 let mongoose = require('mongoose');
 
 let TopicSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: true,
-        unique: true
-    },
     name: String,
     vraag: String,
     opmerkingen: [{type: mongoose.Schema.Types.ObjectId, ref: 'Opmerking'}]
