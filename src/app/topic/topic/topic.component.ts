1adeaa58431fe652f8e38fdc57e460c36159be72
dennisderoose <dennisderoose@hotmail.com>
2017-12-25 15:04:30 +0100

    Opmerking

--- src/app/topic/topic/topic.component.ts
+++ src/app/topic/topic/topic.component.ts
@@ -90,7 +90,7 @@ export class TopicComponent implements OnInit {
         });
         console.log(topic);
         
-        this._topicDataService.updateTopic(topic,topic.id);
+        this._topicDataService.updateTopic(topic,topic.id).subscribe();
 
 
       }
