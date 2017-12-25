4e8daa8268a26f356771d983c97a94109d874848
dennisderoose <dennisderoose@hotmail.com>
2017-12-25 16:51:50 +0100

    Opmerking

--- models/Topic.js
+++ models/Topic.js
@@ -4,7 +4,7 @@ let TopicSchema = new mongoose.Schema({
     id: String,
     name: String,
     vraag: String,
-    opmerkingen: []
+    opmerkingen: [{type: mongoose.Schema.Types.ObjectId, ref: 'Opmerking'}]
 });
 
 mongoose.model('Topic', TopicSchema);
