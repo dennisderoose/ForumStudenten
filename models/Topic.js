994b49e1d77e105f401bb4d2e1bc7e97402e5bce
dennisderoose <dennisderoose@hotmail.com>
2017-12-25 16:08:28 +0100

    Opmerking

--- models/Topic.js
+++ models/Topic.js
@@ -1,12 +1,13 @@
 let mongoose = require('mongoose');
-import { Opmerking } from '../app/topic/opmerking/opmerking.model';
-//import { Opmerking } from 'app/topic/opmerking/opmerking.model';
 
 let TopicSchema = new mongoose.Schema({
     id: String,
     name: String,
     vraag: String,
-    opmerkingen: [Opmerking]
+    opmerkingen: [{
+        _id: String,
+        name: String
+    }]
 });
 
 mongoose.model('Topic', TopicSchema);
