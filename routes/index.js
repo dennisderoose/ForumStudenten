0c66b153fbd73a912944460f439243978e0f7cf6
dennisderoose <dennisderoose@hotmail.com>
2017-12-25 15:49:15 +0100

    Opmerking

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ router.put("/webapptaak/topics/:id", auth, function(req, res) {
   Topic.findOneAndUpdate({
       _id: req.params.id
   },{$set: {
-    opmerkingen: req.body.opmerkingen
+    name = req.body.name
   }}, {upsert: true}, function(err, Topic) {
     if(err) {
       res.status(500).send(err)
