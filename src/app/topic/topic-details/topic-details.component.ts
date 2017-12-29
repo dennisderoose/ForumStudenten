5aea9f9bb3fa20bffd2d8ab7f764d2d9dccc87f0
dennisderoose <dennisderoose@hotmail.com>
2017-12-29 14:09:54 +0100

    Oplossing user

--- src/app/topic/topic-details/topic-details.component.ts
+++ src/app/topic/topic-details/topic-details.component.ts
@@ -41,7 +41,7 @@ export class TopicDetailsComponent implements OnInit {
     const topic = new Topic(this.topic.value.name, this.topic.value.vraag, this.user);
     this._topicDataService.addNewTopic(topic).subscribe(item => {
     });
-    this._router.navigate(['topic']);
+    this._router.navigate(['topic'], { queryParams: { user: this.user} });
   }
 
   
